d10b7f51de6e84d8750a454e46606a509f990f53
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2024-11-24 11:25:18 +0530

    Added sample codes for testing
    Added sample codes for testing

---
+++ Codes/sample_code_corrected.py
@@ -0,0 +1,9 @@
+# sample_code_corrected.py
+
+
+def calculate_sum(num1: int, num2: int) -> int:
+    """Calculate the sum of two integers."""
+    return num1 + num2
+
+
+print("The result is:", calculate_sum(5, 10))
---
+++ Codes/sample_code_with_errors.py
@@ -0,0 +1,8 @@
+# sample_code_with_errors.py
+
+
+def calculateSum(num1, num2):
+    return num1 + num2
+
+
+print("The result is: ", calculateSum(5, 10))
